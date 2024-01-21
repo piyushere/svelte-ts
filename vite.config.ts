838d87cfa506dc16fb0d438b7a763a36ecdcfbbc
piyushere <kingepiyush@gmail.com>
2024-01-21 15:49:18 +0530

    added implicit caching in deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte(), viteCompression(), tsconfigPaths()],
-    base: process.env.BUILD_GITHUB_PAGES === 'true' ? '/svelte-ts' : undefined,
+    base: `/${process.env.BASE_PATH || ''}`,
     server: {
         proxy: {
             '/api': {
