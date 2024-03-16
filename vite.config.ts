ae9692c4f8a0e7a66b9658cac9e55a1edb596318
piyushnice <piyush.kinge@nice.com>
2024-03-16 18:19:09 +0530

    added shadcn-svelte

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import viteCompression from 'vite-plugin-compression';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte(), viteCompression(), tsconfigPaths()],
     base: `/${process.env.BASE_PATH || ''}`,
+    resolve: {
+        alias: {
+            '@lib': path.resolve('./src/lib'),
+        },
+    },
     server: {
         port: 3000,
         proxy: {
